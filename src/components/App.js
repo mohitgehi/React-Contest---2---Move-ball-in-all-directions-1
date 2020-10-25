89304fcf12ac160e77716b6dd629febfe66cffa4
Pratik Patil <38435193+pratik7368patil@users.noreply.github.com>
2020-10-25 18:29:10 +0530

    change 1

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,48 @@ const App = () => {
   const [renderBall, setRenderBall] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [ballPosition,setBallPosition] = useState({
+  const [ballPosition, setBallPosition] = useState({
     left: "0px",
-    top: "0px",
+    top: "0px"
   });
-  const reset = () => {};
-  const renderChoice = () => {};
+
+  const start = () => {
+    setRenderBall(!renderBall);
+  };
+
+  const reset = () => {
+    setBallPosition({
+      left: "0px",
+      top: "0px"
+    });
+  };
+  const renderChoice = () => {
+    if (renderBall) {
+      return <div className="ball" style={ballPosition}></div>;
+    } else return <button onClick={start}>Click For One Ball</button>;
+  };
+
+  const move = (event) => {
+    const copyBallPosition = { ...ballPosition };
+    if (event.keyCode === 39) {
+      copyBallPosition.left = +copyBallPosition.left.slice(0, -2) + 5 + "px";
+    } else if (event.keyCode === 40) {
+      copyBallPosition.top = +copyBallPosition.top.slice(0, -2) + 5 + "px";
+    } else if (event.keyCode === 38) {
+      copyBallPosition.top = +copyBallPosition.top.slice(0, -2) - 5 + "px";
+    } else if (event.keyCode === 37) {
+      copyBallPosition.left = +copyBallPosition.left.slice(0, -2) - 5 + "px";
+    }
+    setBallPosition(copyBallPosition);
+  };
+
+  React.useEffect(() => {
+    document.addEventListener("keydown", move);
+
+    return () => {
+      document.removeEventListener("keydown", move);
+    };
+  });
 
   return (
     <div className="playground">
